89f66ec0f1dd54d67bce639113d0814a5bdd18f5
Matthias Niess <matthias.niess@sh-netz.com>
2023-05-15 12:09:33 +0200

    add example mailable and test route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Mail\OrderShipped;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $mailable = new OrderShipped;
+    Mail::to('example@example.com')
+        ->send($mailable);
+
+    return $mailable;
 });
